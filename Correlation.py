07c343d2755c02596b1bb4959af623786cef9c01
mingyu <mgcha85@gmail.com>
2019-04-12 13:06:42 -0400

    2019-02-25

--- Correlation.py
+++ Correlation.py
@@ -23,16 +23,28 @@ class Correlation:
         strand = {'+': 'plus', '-': 'minus'}
         df_fantom = self.load_fantom()
 
-        fpath = os.path.join(self.root, 'database', 'genecode.db')
+        fpath = os.path.join(self.root, 'database', 'fantom5.db')
         con = sqlite3.connect(fpath)
 
+        contents = []
         for idx in df_fantom.index:
             chromosome = df_fantom.loc[idx, 'chromosome']
             start = df_fantom.loc[idx, 'start']
             end = df_fantom.loc[idx, 'end']
 
-            tname = 'gencode_v28_transcripts_{}_{}'.format(chromosome, strand[df_fantom.loc[idx, 'strand']])
-            df_mir = pd.read_sql_query("SELECT * FROM '{}' WHERE instr(gene, 'MIR')>0".format(tname), con)
+            df_mir = pd.read_sql_query("SELECT * FROM 'human_promoters_wo_duplicates' WHERE chromosome='{}' AND "
+                                       "strand='{}' AND NOT start>{end} AND NOT end<{start}"
+                                       "".format(chromosome, strand, start=start, end=end), con)
+
+            df_gene = pd.read_sql_query("SELECT * FROM 'human_gene' WHERE chromosome='{}' AND strand='{}' AND NOT "
+                                       "start>{end} AND NOT end<{start}"
+                                       "".format(chromosome, strand, start=start, end=end), con)
+            if not df_mir.empty:
+                contents.append('miRNA')
+            elif not df_gene.empty:
+                contents.append('gene')
+            else:
+                contents.append('other')
 
 
 if __name__ == '__main__':
