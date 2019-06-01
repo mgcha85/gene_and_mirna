7de783124fd5253654e5e967baafad26b73f203e
mingyu <mgcha85@gmail.com>
2019-06-01 08:04:24 -0400

    2019-02-25

--- exon/histone_features.py
+++ exon/histone_features.py
@@ -3,7 +3,7 @@ import sqlite3
 import pandas as pd
 import socket
 import pickle as pkl
-
+import numpy as np
 from XmlHandler import XmlHandler
 from histogram_gpu import histogram_gpu
 
@@ -42,6 +42,31 @@ class histone_features:
                                  "NOT chromosome='chrY'".format(fid[0]), con)
         return df[df['chromosome'].str.len() <= 5].reset_index(drop=True)
 
+    def get_hist(self, df_ref, rsc_path, fname, BINSIZE=100):
+        con = sqlite3.connect(rsc_path)
+
+        hist_out = []
+        for idx in df_ref.index:
+            chromosome = df_ref.loc[idx, 'chromosome']
+            start = df_ref.loc[idx, 'start']
+            end = df_ref.loc[idx, 'end']
+            df__ = pd.read_sql_query("SELECT * FROM '{}_{}' WHERE NOT start>{end} AND NOT end<{start}".format(fname, chromosome, start=start, end=end), con)
+
+            bin_width = (end - start + BINSIZE) // BINSIZE
+            hist = np.zeros(bin_width)
+            if df__.empty:
+                hist_out.append(hist)
+                continue
+
+            bin_start = start
+            for i in range(bin_width):
+                bin_end = bin_start + BINSIZE
+                df = df__[~(~(df__['start'] > bin_end) & ~(df__['end'] < bin_start))]
+                hist[i] = df.shape[0]
+                bin_start += BINSIZE
+            hist_out.append(hist)
+        return hist_out
+
     def run(self):
         import matplotlib.pyplot as plt
 
@@ -58,9 +83,8 @@ class histone_features:
             dfs = {}
             for histone, fname in fnames.items():
                 rsc_path = os.path.join(dirname__, tissue, 'histone.db')
-                df = hgpu.histogram_gpu(df_ref, rsc_path, fname)
-                dfs[histone] = df
-                return
+                hist_out = self.get_hist(df_ref, rsc_path, fname)
+                print(hist_out)
 
             # for idx in df_ref.index:
             #     gene_name = df_ref.loc[idx, 'gene_name']
