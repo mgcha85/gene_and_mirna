ad18cac46de4f80b2377a4f953464f1dfdb22312
mingyu <mgcha85@gmail.com>
2019-06-21 11:13:31 -0400

    2019-02-25

--- Correlation2.py
+++ Correlation2.py
@@ -521,15 +521,14 @@ if __name__ == '__main__':
         cor.to_server()
 
     else:
-        for band in [100, 500]:
-            cor.get_scores_by_tissues(band)
-            cor.correlation_mir_gene(band)
-
-        cor.rna_unique_gene()
+        # cor.rna_unique_gene()
         for band in [100, 500]:
             comp.fantom_to_gene(band)
             comp.fantom_to_mir(band)
+
             cor.band = band
+            cor.get_vector('fantom_cage_by_tissue_{}.db'.format(band))
+            cor.get_vector('fantom_cage_by_tissue_mir_{}.db'.format(band))
 
             for cluster_size in [20, 100]:
                 cor.csize = cluster_size
@@ -540,5 +539,5 @@ if __name__ == '__main__':
                     cor.correlation()
                 cor.merge_versions()
 
-            cor.get_vector('fantom_cage_by_tissue_mir_{}.db'.format(band))
-            cor.get_vector('fantom_cage_by_tissue_{}.db'.format(band))
+            cor.get_scores_by_tissues(band)
+            cor.correlation_mir_gene(band)
