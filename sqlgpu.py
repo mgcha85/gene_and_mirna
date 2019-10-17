3183d3d6dd761315972c2e4de782272364348b87
= <mgcha85@gmail.com>
2019-10-17 14:36:58 -0400

    Laptop

--- sqlgpu.py
+++ sqlgpu.py
@@ -35,7 +35,7 @@ __device__ int binary_search(const int *X, const int val, const int N, const int
     return -1;
 }
 
-__device__ int binary_search_loc(const int *X, const int ref_val, const int col, const int N)
+__device__ int binary_search_loc(const int *X, const int val, const int col, const int N)
 {
     // X: array, val: nth value, N: length of X, col: columns number
     // if X has no val, return -1 
@@ -58,8 +58,8 @@ __device__ int binary_search_loc(const int *X, const int ref_val, const int col,
         else                    end = mid - 1;
         
         if(start>=end) {
-            if(val > res_val) start++;
-            return start;
+            if(val < res_val) mid++;
+            return mid;
         }
     }
     return -1;
@@ -79,9 +79,6 @@ __device__ void get_overlap(const int *X, const int ref_start, const int ref_end
         if(!(res_start > ref_end) && !(res_end < ref_start)) {
             if(offset < 0) offset = i;
             num_ele++;
-            
-            if(idx == 20)
-                printf("[%d] [%d/ %d] [%d, %d] res_start: %d, res_end: %d, ref_start: %d, ref_end: %d\\n", idx, i, N, offset, num_ele, res_start, res_end, ref_start, ref_end);
         }
         if(ref_end < res_start) break;
     }
@@ -91,13 +88,12 @@ __device__ void get_overlap(const int *X, const int ref_start, const int ref_end
     addr[WIDTH * idx + END] = offset + num_ele - 1;    
 }
 
-__device__ float get_sum(const int *X, const int ref_start, const int ref_end, const float *score, const int N, const int idx)
+__device__ float get_sum(const int *X, const int ref_start, const int ref_end, const float *score, const int offset, const int N, const int idx)
 {
     float sum = 0.0;
-    
     if((X[START] > ref_end) || (X[WIDTH * (N - 1) + END] < ref_start)) return -1;
     
-    for(int i=0; i<N; i++) {
+    for(int i=offset; i<N; i++) {
         int res_start = X[WIDTH * i + START];
         int res_end = X[WIDTH * i + END];
         
@@ -132,21 +128,26 @@ __global__ void cuda_sql(const int *ref, const int *res, float *score, float *ou
     int ref_start = ref[idx * WIDTH + START];
     int ref_end = ref[idx * WIDTH + END];
     
-    out[idx] = get_sum(res, ref_start, ref_end, score, M, idx);
+    out[idx] = get_sum(res, ref_start, ref_end, score, 0, M, idx);
     //get_overlap(res, ref_start, ref_end, addr, M, idx);
     //cuda_sum(score, addr, out, N, idx);
 }
 
-__global__ void cuda_sql2(const int *ref, const int *res, int *addr, const int N, const int M)
+__global__ void cuda_sql2(const int *ref, const int *res, float *score, float *out, const int N, const int M)
 {
     const int idx = threadIdx.x + blockIdx.x * blockDim.x;
     if (idx >= N) return;
-    
+
+    int offset, offset1, offset2;    
     int ref_start = ref[idx * WIDTH + START];
     int ref_end = ref[idx * WIDTH + END];
     
-    addr[WIDTH * idx + START] = binary_search_loc(res, ref_end, START, M);
-    addr[WIDTH * idx + END] = binary_search_loc(res, ref_start, END, M) - 1;
+    offset1 = binary_search_loc(res, ref_end, START, M)-1;
+    offset2 = binary_search_loc(res, ref_start, END, M)-1;
+    if(offset1 < offset2)   offset=offset1;
+    else                    offset=offset2;
+    
+    out[idx] = get_sum(res, ref_start, ref_end, score, offset, M, idx);
 }
 
 """)
@@ -167,7 +168,7 @@ class Sqlgpu:
             out.loc[idx, 'end'] = df_res_sub.index[-1]
         return out
 
-    def addr_run(self, df_ref, df_res):
+    def bin_run(self, df_ref, df_res):
         # df_ref, df_res should be DataFrame
         # they should have start, end only
         # they should be separated by chromosome and strand
@@ -188,21 +189,20 @@ class Sqlgpu:
         N = np.int32(df_ref.shape[0])
         M = np.int32(df_res.shape[0])
 
-        # output
-        addr = (-1 * np.ones(N * 2)).astype(np.int32)
-        addr_gpu = cuda.mem_alloc(addr.nbytes)
-        cuda.memcpy_htod(addr_gpu, addr)
+        score = df_res['score'].values.astype(np.float32)
+        score_gpu = cuda.mem_alloc(score.nbytes)
+        cuda.memcpy_htod(score_gpu, score)
 
-        out = np.zeros(N).astype(np.float32)
+        out = np.random.random(N).astype(np.float32)
         out_gpu = cuda.mem_alloc(out.nbytes)
         cuda.memcpy_htod(out_gpu, out)
 
         gridN = int((N + THREADS_PER_BLOCK - 1) // THREADS_PER_BLOCK)
 
-        func = mod.get_function("cuda_sql")
-        func(ref_gpu, res_gpu, out_gpu, N, M, block=(THREADS_PER_BLOCK, 1, 1), grid=(gridN, 1))
-        cuda.memcpy_dtoh(addr, addr_gpu)
-        return addr
+        func = mod.get_function("cuda_sql2")
+        func(ref_gpu, res_gpu, score_gpu, out_gpu, N, M, block=(THREADS_PER_BLOCK, 1, 1), grid=(gridN, 1))
+        cuda.memcpy_dtoh(out, out_gpu)
+        return out
 
     def run(self, df_ref, df_res):
         # df_ref, df_res should be DataFrame
@@ -251,7 +251,8 @@ class Sqlgpu:
 if __name__ == '__main__':
     import socket, os, sqlite3
     from Database import Database
-
+    from copy import deepcopy
+    
     hostname = socket.gethostname()
     if hostname == 'mingyu-Precision-Tower-7810':
         root = '/media/mingyu/70d1e04c-943d-4a45-bff0-f95f62408599/Bioinformatics'
@@ -287,12 +288,21 @@ if __name__ == '__main__':
         print(tname)
 
         df_ref = pd.read_sql_query("SELECT start, end FROM '{}'".format(tname), con_ref)
-        df_rep = pd.DataFrame(index=df_ref.index, columns=['ref_start', 'ref_end', 'start', 'end'])
+        if strand == '+':
+            tss = deepcopy(df_ref['start'])
+        else:
+            tss = deepcopy(df_ref['end'])
+
+        df_ref['start'] = tss - 500
+        df_ref['end'] = tss + 500
+
+        df_rep = pd.DataFrame(index=df_ref.index, columns=['ref_start', 'ref_end', 'score'])
         df_rep[['ref_start', 'ref_end']] = df_ref[['start', 'end']]
         for tissue in tissues:
             df_res = pd.read_sql_query("SELECT start, end, FPKM FROM '{}_{}_{}'".format(tissue, chromosome, strand), con_rna)
             df_res = df_res.rename(columns={"FPKM": "score"})
-            df_rep.loc[:, ['start', 'end']] = sqlgpu.addr_run(df_ref, df_res)
+            temp = sqlgpu.bin_run(df_ref, df_res)
+            df_rep.loc[:, 'score'] = temp
             df_rep.to_excel(writer, sheet_name=tissue, index=None)
         writer.save()
         writer.close()
